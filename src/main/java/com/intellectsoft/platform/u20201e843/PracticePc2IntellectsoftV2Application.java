d3a1aad1b6a42a8039c1f03ee05f748f7afca0e5
Salim Ramirez <salimramirezm@gmail.com>
2025-06-26 00:39:43 -0500

    feat: update main class with auditing

--- src/main/java/com/intellectsoft/platform/u20201e843/PracticePc2IntellectsoftV2Application.java
+++ src/main/java/com/intellectsoft/platform/u20201e843/PracticePc2IntellectsoftV2Application.java
@@ -2,7 +2,9 @@ package com.intellectsoft.platform.u20201e843;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PracticePc2IntellectsoftV2Application {
 
