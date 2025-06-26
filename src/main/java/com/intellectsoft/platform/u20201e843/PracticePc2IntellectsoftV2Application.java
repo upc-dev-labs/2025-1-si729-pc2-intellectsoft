99e59867be0c571cb1275850c04c7c725aab708c
Salim Ramirez <salimramirezm@gmail.com>
2025-06-26 00:29:57 -0500

    chore: initial commit

---
+++ src/main/java/com/intellectsoft/platform/u20201e843/PracticePc2IntellectsoftV2Application.java
@@ -0,0 +1,13 @@
+package com.intellectsoft.platform.u20201e843;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticePc2IntellectsoftV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticePc2IntellectsoftV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/intellectsoft/platform/u20201e843/PracticePc2IntellectsoftV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellectsoft.platform.u20201e843;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticePc2IntellectsoftV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
